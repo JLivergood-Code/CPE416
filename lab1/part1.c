0dea9e6e4ba513e03eaf20c18cd3393a22737b46
dannym49 <97007381+dannym49@users.noreply.github.com>
2025-04-07 09:55:15 -0700

    Started part1.c

--- lab1/part1.c
+++ lab1/part1.c
@@ -1,7 +1,79 @@
-#include "part1.h"
+#include "globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
+void LED_fade(void) {
+    while(1){
+        led_on(1);
+        _delay_ms(1);  
+        led_off(1);
+        _delay_ms(20);  
+        led_on(1);
+        _delay_ms(2);
+        led_off(1);
+        _delay_ms(19);  
+        led_on(1);
+        _delay_ms(3);
+        led_off(1);
+        _delay_ms(18);  
+        led_on(1);
+        _delay_ms(4);
+        led_off(1);
+        _delay_ms(17);  
+        led_on(1);
+        _delay_ms(5);
+        led_off(1);
+        _delay_ms(16);
+    
+        
+        
+        led_on(1);
+        _delay_ms(500);
+    
+    
+        led_off(1);
+        _delay_ms(1);
+        led_on(1);
+        _delay_ms(20);
+        led_off(1);
+        _delay_ms(2);
+        led_on(1);
+        _delay_ms(19); 
+        led_off(1);
+        _delay_ms(3);
+        led_on(1);
+        _delay_ms(18); 
+        led_off(1);
+        _delay_ms(4);
+        led_on(1);
+        _delay_ms(17);
+        led_off(1);
+        led_on(1);
+        _delay_ms(16); 
+        led_off(1);
+        _delay_ms(5);
+        led_on(1);
+        _delay_ms(15); 
+        led_off(1);
+        _delay_ms(6);
+        led_on(1);
+        _delay_ms(14); 
+        led_off(1);
+        _delay_ms(7);
+        led_on(1);
+        _delay_ms(13); 
+        led_off(1);
+        _delay_ms(8);
+    }
+    
+    
+ }
 
-void part1(){
-	
-
+int main(void) {
+   init();  //initialize board hardware
+   LED_fade();  //fade LED1
+   return 0;
 }
+
+
