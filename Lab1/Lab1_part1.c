1493a177d17007d8b321f840fa6162d55be72e01
dannym49 <dmoren32@calpoly.edu>
2025-04-14 09:37:45 -0700

    Lab2

--- Lab1/Lab1_part1.c
+++ Lab1/Lab1_part1.c
@@ -4,76 +4,42 @@
 #include <avr/interrupt.h>
 
 void LED_fade(void) {
-    while(1){
-        led_on(1);
-        _delay_ms(1);  
-        led_off(1);
-        _delay_ms(20);  
-        led_on(1);
-        _delay_ms(2);
-        led_off(1);
-        _delay_ms(19);  
-        led_on(1);
-        _delay_ms(3);
-        led_off(1);
-        _delay_ms(18);  
-        led_on(1);
-        _delay_ms(4);
-        led_off(1);
-        _delay_ms(17);  
-        led_on(1);
-        _delay_ms(5);
-        led_off(1);
-        _delay_ms(16);
-    
-        
-        
+    while (1) {
+        //Fade In
+        for (int brightness = 0; brightness <= 20; brightness++) {
+            led_on(1);
+            for (int i = 0; i < brightness; i++) {
+                _delay_ms(1); //Time that LED is on
+            }
+            led_off(1);
+            for (int i = 0; i < (20 - brightness); i++) {
+                _delay_ms(1); //Time that LED is off
+            }
+        }
+        //Stay on for a moment
         led_on(1);
         _delay_ms(500);
-    
-    
         led_off(1);
-        _delay_ms(1);
-        led_on(1);
-        _delay_ms(20);
-        led_off(1);
-        _delay_ms(2);
-        led_on(1);
-        _delay_ms(19); 
-        led_off(1);
-        _delay_ms(3);
-        led_on(1);
-        _delay_ms(18); 
-        led_off(1);
-        _delay_ms(4);
-        led_on(1);
-        _delay_ms(17);
-        led_off(1);
-        led_on(1);
-        _delay_ms(16); 
-        led_off(1);
-        _delay_ms(5);
-        led_on(1);
-        _delay_ms(15); 
-        led_off(1);
-        _delay_ms(6);
-        led_on(1);
-        _delay_ms(14); 
-        led_off(1);
-        _delay_ms(7);
-        led_on(1);
-        _delay_ms(13); 
-        led_off(1);
-        _delay_ms(8);
-    }
-    
-    
- }
 
-int main(void) {
-   init();  //initialize board hardware
-   LED_fade();  //fade LED1
-   return 0;
+        //Fade Out
+        for (int brightness = 20; brightness >= 0; brightness--) {
+            led_on(1);
+            for (int i = 0; i < brightness; i++) {
+                _delay_ms(1); //Time that LED is on
+            }
+            led_off(1);
+            for (int i = 0; i < (20 - brightness); i++) {
+                _delay_ms(1); //Time that LED is off
+            }
+        }
+
+        //Stay off for a moment
+        _delay_ms(500);
+    }
 }
 
-
+int main(void) {
+    init();  //initialize board hardware
+    LED_fade();  //Fade LED1
+    return 0;
+}
