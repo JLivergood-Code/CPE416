a902fdd47a45ec6b8a0cf81898c2edc5ab21b174
dannym49 <dmoren32@calpoly.edu>
2025-04-16 14:05:43 -0700

    Fear and aggression done, just need to implement button changes

--- Lab2/Lab2_part2.c
+++ Lab2/Lab2_part2.c
@@ -57,9 +57,60 @@ void fear(void) {
     
 }
 
+void aggression(void) {
+    char buffer[16];
+    int servo0_position, servo1_position;
+    u16 light_0, light_1;
+    u08 button_pressed = 0; // Initialize button_pressed to 0 (fear mode)
+    while(button_pressed == 0){
+        light_0 = analog(ANALOG0_PIN);
+        light_1 = analog(ANALOG1_PIN);
+        _delay_ms(300);          // Small delay so it's readable
+        
+        if(light_0 < light_1) {
+            // Move left
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 - 220) * 30 / (240 - 220);
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position );
+            
+            snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // Display it on the LCD
+            clear_screen();         // Clear previous content
+            lcd_cursor(0, 0);        // Top-left of LCD
+            print_string(buffer);
+            snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            lcd_cursor(0,1);        // Bottom-left of LCD
+            print_string(buffer);
+            lcd_cursor(0, 7);
+            print_string("L");
+        } else {
+            // Move right
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 * 30) / 255;
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position);
+            snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // Display it on the LCD
+            clear_screen();         // Clear previous content
+            lcd_cursor(0, 0);        // Top-left of LCD
+            print_string(buffer);
+            snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            lcd_cursor(0,1);        // Bottom-left of LCD
+            print_string(buffer);
+            lcd_cursor(0, 7);
+            print_string("R");
+        }
+    }
+    
+}
+
 int main(void) {
-    init();        // Initialize board hardware (LCD, ADC, etc.)
-    init_adc();    // Initialize ADC subsystem (important!)
-    fear();
+    init();        
+    init_adc();    
+    //fear();
+    aggression();
     return 0;
 }
