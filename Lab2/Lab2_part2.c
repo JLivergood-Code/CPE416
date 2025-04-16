a820f28c832998158740ea115d7479030229e6c2
dannym49 <dmoren32@calpoly.edu>
2025-04-16 16:28:06 -0700

    mode switching done

--- Lab2/Lab2_part2.c
+++ Lab2/Lab2_part2.c
@@ -4,8 +4,108 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 
-//button_pressed = 0->fear
-//button_pressed = 1->aggression
+
+
+void braitenberg(void){
+    char buffer[16];
+    int servo0_position, servo1_position;
+    u16 light_0, light_1;
+    static u08 mode = 0; // 0 = fear, 1 = aggression
+    if (get_btn() == 1 && mode == 0) {
+        mode = 1; // Switch to aggression mode
+    } else if (get_btn() == 1 && mode == 1) {
+        mode = 0; // Switch to fear mode
+    }
+    if (mode == 0) {//Fear mode
+        
+        light_0 = analog(ANALOG0_PIN);
+        light_1 = analog(ANALOG1_PIN);
+        _delay_ms(300);          // Small delay so it's readable
+        clear_screen();         // Clear previous content
+        lcd_cursor(0, 0);
+        print_string("F Mode");
+        if(light_0 < light_1) {
+            // Move left
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 - 220) * 30 / (240 - 220);
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position );
+            
+            // snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // // Display it on the LCD
+            // clear_screen();         // Clear previous content
+            // lcd_cursor(0, 0);        // Top-left of LCD
+            // print_string(buffer);
+            // snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            // lcd_cursor(0,1);        // Bottom-left of LCD
+            // print_string(buffer);
+            // lcd_cursor(0, 7);
+            // print_string("L");
+        } else {
+            // Move right
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 * 30) / 255;
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position);
+            // snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // // Display it on the LCD
+            // clear_screen();         // Clear previous content
+            // lcd_cursor(0, 0);        // Top-left of LCD
+            // print_string(buffer);
+            // snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            // lcd_cursor(0,1);        // Bottom-left of LCD
+            // print_string(buffer);
+            // lcd_cursor(0, 7);
+            // print_string("R");
+        }
+    } else if (mode == 1) {
+        light_0 = analog(ANALOG0_PIN);
+        light_1 = analog(ANALOG1_PIN);
+        _delay_ms(300);          // Small delay so it's readable
+        clear_screen();         // Clear previous content
+            lcd_cursor(0, 0);
+            print_string("A Mode");
+        if(light_0 > light_1) {
+            // Move left
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 - 220) * 30 / (240 - 220);
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position );
+            
+            // snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // // Display it on the LCD
+            // clear_screen();         // Clear previous content
+            // lcd_cursor(0, 0);        // Top-left of LCD
+            // print_string(buffer);
+            // snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            // lcd_cursor(0,1);        // Bottom-left of LCD
+            // print_string(buffer);
+            // lcd_cursor(0, 7);
+            // print_string("L");
+        } else {
+            // Move right
+            servo0_position = (light_0 - 220) * 30 / (240 - 220) + 127;
+            servo1_position = 127 - (light_1 * 30) / 255;
+            set_servo(0, servo0_position);
+            set_servo(1, servo1_position);
+            // snprintf(buffer, sizeof(buffer), "0 %d", servo0_position);
+    
+            // // Display it on the LCD
+            // clear_screen();         // Clear previous content
+            // lcd_cursor(0, 0);        // Top-left of LCD
+            // print_string(buffer);
+            // snprintf(buffer, sizeof(buffer), "1 %d", servo1_position);
+            // lcd_cursor(0,1);        // Bottom-left of LCD
+            // print_string(buffer);
+            // lcd_cursor(0, 7);
+            // print_string("R");
+            
+        }
+    }
+}
 
 void fear(void) {
     char buffer[16];
@@ -111,6 +211,11 @@ int main(void) {
     init();        
     init_adc();    
     //fear();
-    aggression();
+    //aggression();
+    while(1) {
+        braitenberg();
+    }
+    
+   
     return 0;
 }
