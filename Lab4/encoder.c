29fd9f583559d3858fbfddfb81d8510aa79489ce
joshua livergood <joshua.livergood@gmail.com>
2025-05-19 09:48:10 -0700

    Got turning working

--- Lab4/encoder.c
+++ Lab4/encoder.c
@@ -44,6 +44,32 @@ void motor(uint8_t num, int8_t speed) {
     set_servo(num, servo_position);
 }
 
+void turn(int8_t side, u08 degrees){
+    // if side == 1, robot turns right,if side == -1, robot turns left
+    int8_t left_dir = 1 * side; 
+    int8_t right_dir = -1 * side;
+
+    u08 num_ticks = 59;
+
+    num_ticks = num_ticks * ((float) degrees / 180) + 2;
+
+    left_encoder = 0;
+    right_encoder = 0;
+
+    while(left_encoder < num_ticks && right_encoder < num_ticks){
+        motor(0, 20*left_dir);
+        motor(1, 20*right_dir);
+
+        lcd_cursor(0, 1);
+        print_num(left_encoder);
+
+    }
+
+    motor(0, 0);
+    motor(1, 0);
+
+}
+
 void run_encoder(){
     
     lcd_cursor(0, 0);
@@ -55,33 +81,38 @@ void run_encoder(){
     //     motor(1, 0);//right
 
     // }
-     while(left_encoder < 100 && right_encoder < 100 ){//run until 100 ticks
-        lcd_cursor(0, 1);
-        print_num(left_encoder);
-        lcd_cursor(5, 1);
-        print_num(right_encoder);
-        motor(0, 20); //left
-        motor(1, 20);//right
-        
-    }
-    left_encoder = right_encoder = 0;
-    //stop motors after 100 ticks
 
-    //turning stuff
-    while(left_encoder < 15 && right_encoder < 15 ){//run until 100 ticks
-        lcd_cursor(0, 1);
-        print_num(left_encoder);
-        lcd_cursor(5, 1);
-        print_num(right_encoder);
-        motor(0, -20); //left
-        motor(1, 20);//right
-        
+    for(u08 i = 0; i < 4; i++){
+        u08 num_ticks = 108;
+
+        left_encoder = right_encoder = 0;
+
+        while(left_encoder < num_ticks && right_encoder < num_ticks ){//run until 100 ticks
+            lcd_cursor(0, 1);
+            print_num(left_encoder);
+            lcd_cursor(5, 1);
+            print_num(right_encoder);
+            motor(0, 20); //left
+            motor(1, 20);//right
+            
+        }
+        left_encoder = right_encoder = 0;
+        //stop motors after 100 ticks
+
+        //turning stuff
+        turn(-1, 90);
     }
 }
 
 int main(void) {
     init();  //initialize board hardware
     init_encoder(); //initialize encoder
+    
+    motor(0, 0);
+    motor(1, 0);
+
+    while(get_btn() == 0);
+
     run_encoder(); //run encoder
     
     return 0;
