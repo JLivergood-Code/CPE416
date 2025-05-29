eb4a08559f212e6df5834fcc7420588fc5b494c1
Joshua Livergood <joshua.livergood@gmail.com>
2025-05-29 00:13:17 -0700

    Initial Brainstorming and Tracking Code

--- final/ir_sensor.c
+++ final/ir_sensor.c
@@ -4,6 +4,24 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 
+#define DELTA_LARGE 3
+#define DELTA_FINE 1
+#define RANGE_SERVO 2
+
+void motor(uint8_t num, int8_t speed) {
+    if (num == 1) {
+        speed = -speed;
+    }
+    int servo_position = 0.3 * speed + 127;
+    if (servo_position < 97) {
+        servo_position = 97;
+    }
+    if (servo_position > 157) {
+        servo_position = 157;
+    }
+    set_servo(num, servo_position);
+}
+
 void read_ir(){
 
     int analog1 = analog(ANALOG0_PIN);
@@ -21,7 +39,6 @@ void read_ir(){
 void swivel_motor(){
     int degrees = 0;
     int direction = 1;
-    const int change = 3;
 
     while(1){
         if(degrees <= 5){
@@ -31,7 +48,7 @@ void swivel_motor(){
             direction = -1;
         }
 
-        degrees = degrees + (direction * change);
+        degrees = degrees + (direction * DEGREE_CHANGE);
 
         // read_ir();
 
@@ -49,19 +66,91 @@ void swivel_motor(){
 
 }
 
-void motor(uint8_t num, int8_t speed) {
-    if (num == 1) {
-        speed = -speed;
+int8_t get_closest(){
+
+    u08 degrees = 0;
+    u08 range = 0;
+
+    u08 min_range = -1;
+    u08 min_range_deg = 0;
+    // int8_t direction = 1;
+
+    while(degrees <= 180){
+        range = analog(ANALOG0_PIN);
+
+        if(range < min_range){
+            min_range = range;
+            min_range_deg = degrees;
+        }
+
+        degrees += DELTA_LARGE;
     }
-    int servo_position = 0.3 * speed + 127;
-    if (servo_position < 97) {
-        servo_position = 97;
-    }
-    if (servo_position > 157) {
-        servo_position = 157;
-    }
-    set_servo(num, servo_position);
+
+    // typecasts to signed int, returns degrees from center
+    return ((int8_t) degrees - 90);   
+
 }
+
+/*
+Brainstorm:
+- initial sweep, finds the closest object and starts tracking
+    - tracking involves slightly moving the sensor left and right
+    - if the object is closer on the left or right, move the robot that direction
+
+- while the sensor tracks, the robot uses a PID controller to follow the sensor
+
+- all of this is in a while loop
+    - 1 forever loop (or until button press to reset)
+        - 1 inside loop that goes until the light sensors detect edge, then turn around and repeat
+
+
+*/
+
+void on_line(){
+    return false;
+}
+
+void track(){
+    int8_t closest_deg = 0;
+    u08 closest_range = 0;
+    int8_t dif_center = 0;
+    u08 cur_range = 0;
+    u08 cur_degree = 0;
+    int8_t dir = 1;
+
+    while(1){
+        closest_deg = get_closest();
+        cur_degree = closest_deg;
+        set_servo(RANGE_SERVO, closest_deg);
+
+        closest_range = analog(ANALOG0_PIN);
+
+        while(!on_line()){
+            cur_range = analog(ANALOG0_PIN);
+
+            // if current reading is larger, switch swivel directions
+            if(cur_range > closest_range){
+              dir *= -1;  
+              
+            } 
+
+            // continue to sweep motor
+            cur_degree += dir;
+            set_servo(RANGE_SERVO, closest_deg);
+
+            dif_center = cur_degree - 90
+
+            // PID Controller that takes in difference from center and adjusts motors accordingly
+            motor_pid(dif_center);
+        }
+
+    }
+
+
+
+}
+
+
 
 int main(){
     init();
